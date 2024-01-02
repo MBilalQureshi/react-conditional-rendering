3db56e10b21ba57d7a57558857489d073763f640
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-01-02 18:08:27 +0000

    Use ternary condition to write multiline jsx based on condiotions

--- my-app/src/components/ConditionalRenderingClass.js
+++ my-app/src/components/ConditionalRenderingClass.js
@@ -4,7 +4,8 @@ export class ConditionalRenderingClass extends Component {
     constructor(props) {
         super(props)
         this.state = {
-           isLoaded : false,
+           isLoaded : true,
+           isLoggedIn : true,
         }
       }
   render() {
@@ -13,6 +14,18 @@ export class ConditionalRenderingClass extends Component {
             <h1>
                 {this.state.isLoaded ? 'Data loaded' : 'Loading...'}
             </h1>
+
+            {/* write html inside () first () will be loaded if true else second () will render */}
+            {this.state.isLoggedIn ? (
+                <div>
+                    <p>Welcome, please complete steps</p>
+                    <ol>
+                        <li>Email</li>
+                        <li>Profile</li>
+                        <li>Subscribe to newsletter</li>
+                    </ol>
+                </div>
+            ) : (<p>Please Sign in</p>)}
         </div>
     )
   }
